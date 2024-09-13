fd7e975ebbc6dacef78e33ae38afa2bf4897010b
Igorajs <igorajs14@gmail.com>
2024-09-13 22:31:21 +0200

    Testing bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <button className="btn btn-danger">Przycisk</button>
+    </>
+  );
 }
 
 export default App;
